babfe57570fca80e8747c233329974caad0869ae
adorsk <adorsk@gmail.com>
2014-01-27 10:45:59 -0500

    basic template, drum tap events

--- src/FeelTheBeatExerciseView.js
+++ src/FeelTheBeatExerciseView.js
@@ -1,7 +1,48 @@
 define(function(require){
   var Marionette = require('marionette');
+  var Handlebars = require('handlebars');
+
+  var FeelTheBeatExerciseViewTemplate = require("text!./templates/FeelTheBeatExerciseView.html");
 
   var FeelTheBeatExerciseView = Marionette.ItemView.extend({
+    template: Handlebars.compile(FeelTheBeatExerciseViewTemplate),
+
+    ui: {
+      drum: '.drum'
+    },
+
+    events: {
+      "touchstart @ui.drum": "onDrumTapDown",
+      "mousedown @ui.drum": "onDrumTapDown",
+      "touchend @ui.drum": "onDrumTapUp",
+      "mouseup @ui.drum": "onDrumTapUp",
+      "keydown": "onKeyDown",
+      "keyup": "onKeyUp",
+    },
+
+    onDrumTapDown: function(){
+      console.log('tap down');
+    },
+    
+    onDrumTapUp: function(){
+      console.log('tap up');
+    },
+
+    onKeyDown: function(e){
+      // Spacebar.
+      if (e.keyCode == 32){
+        this.onDrumTapDown();
+      }
+    },
+
+    onKeyUp: function(e){
+      // Spacebar.
+      if (e.keyCode == 32){
+        this.onDrumTapUp();
+      }
+    }
+
+
   });
 
   return FeelTheBeatExerciseView;
--- test/specs/FeelTheBeatExerciseView.spec.js
+++ test/specs/FeelTheBeatExerciseView.spec.js
@@ -1,5 +1,17 @@
 define(function(require){
+  var $ = require('jquery');
+  var Backbone = require('backbone');
+
   var FeelTheBeatExerciseView = require('feelTheBeat/FeelTheBeatExerciseView');
+
+  var generateExerciseView = function(){
+    var model = new Backbone.Model({});
+    var view = new FeelTheBeatExerciseView({
+      model: model
+    });
+
+    return view;
+  };
 
   describe("FeelTheBeatExerciseView", function(){
     it('should be defined', function(){
@@ -8,24 +20,82 @@ define(function(require){
 
     describe('after rendered', function(){
 
-      it('should show the instructions', function(){
-        this.fail('NOT IMPLEMENTED');
+      var view;
+      beforeEach(function(){
+        view = generateExerciseView();
+        view.render();
       });
 
-      it('should show the drum', function(){
-        this.fail('NOT IMPLEMENTED');
+      afterEach(function(){
+        view.remove();
       });
 
-      it('should change the drum class on tap', function(){
-        this.fail('NOT IMPLEMENTED');
+      describe('ui elements', function(){
+        it('should show the instructions', function(){
+          expect(view.$el.find('.instructions').length).toBe(1);
+        });
+
+        it('should show the drum', function(){
+          expect(view.$el.find('.drum').length).toBe(1);
+        });
       });
 
-      it('should start the beat when the drum is tapped', function(){
-        this.fail('NOT IMPLEMENTED');
+      describe('tap/mouse/click events', function(){
+        beforeEach(function(){
+          spyOn(view, 'onDrumTapDown');
+          spyOn(view, 'onDrumTapUp');
+          view.delegateEvents();
+        });
+
+        it("should call onDrumTapDown for touchstart", function(){
+          view.ui.drum.trigger('touchstart');
+          expect(view.onDrumTapDown).toHaveBeenCalled();
+        });
+
+        it("should call onDrumTapUpfor touchend", function(){
+          view.ui.drum.trigger('touchend');
+          expect(view.onDrumTapUp).toHaveBeenCalled();
+        });
+
+        it("should call onDrumTapDown for mousedown", function(){
+          view.ui.drum.trigger('mousedown');
+          expect(view.onDrumTapDown).toHaveBeenCalled();
+        });
+
+        it("should call onDrumTapUpfor mouseup", function(){
+          view.ui.drum.trigger('mouseup');
+          expect(view.onDrumTapUp).toHaveBeenCalled();
+        });
+
+        it("should call onDrumTapDown for spacebar down", function(){
+          var e = $.Event("keydown");
+          e.keyCode = 32;
+          view.$el.trigger(e);
+          expect(view.onDrumTapDown).toHaveBeenCalled();
+        });
+
+        it("should call onDrumTapUpfor spacebar up", function(){
+          var e = $.Event("keyup");
+          e.keyCode = 32;
+          view.$el.trigger(e);
+          expect(view.onDrumTapUp).toHaveBeenCalled();
+        });
       });
 
-      it('should trigger a sound event for the first tap the first time', function(){
-        this.fail('NOT IMPLEMENTED');
+      describe('before the first tap', function(){
+
+        it('should change the drum class on tap', function(){
+          this.fail('NOT IMPLEMENTED');
+        });
+
+        it('should start the beat when the drum is tapped', function(){
+          this.fail('NOT IMPLEMENTED');
+        });
+
+        it('should trigger a sound event for the first tap the first time', function(){
+          this.fail('NOT IMPLEMENTED');
+        });
+
       });
 
       it('should trigger beat events until the number of beats is done', function(){
